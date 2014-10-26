2b8aff86cbefb88956dc4ab0ee87deba150eb2df
Pavol Rajzak <rapasoft@gmail.com>
2014-10-26 11:37:00 +0100

    Added JavaDoc description for the interface

--- src/main/java/sk/erni/codingbeer/CodingBeer.java
+++ src/main/java/sk/erni/codingbeer/CodingBeer.java
@@ -4,16 +4,38 @@ import java.util.Map;
 
 public interface CodingBeer {
 
+    /**
+     * Count all cities that are present in the world database.
+     * @return number of all cities in the "world"
+     */
     public int countAllCities();
 
+    /**
+     * @return the name of the single most inhabited city in the World
+     *
+     */
     public String fetchCityWithMostInhabitants();
 
+    /**
+     * @return a Map that has country name as a key and capital city as a value
+     */
     public Map<String, String> fetchEuropeanCountriesWithCapitals();
 
+    /**
+     * Calculate the sum of all people speaking chines based on the percentage
+     * of people speaking chinese per country.
+     */
     public int numberOfPeopleSpeakingChinese();
 
+    /**
+     * Add a new Person into database.
+     */
     void addPerson(String name, Integer age, Integer city);
 
+    /**
+     * @return the list of names of people that are the oldest.
+     * E.g. more people can have the same maximum age.
+     */
     public java.util.List<String> findOldestPeopleNames();
 
 }
